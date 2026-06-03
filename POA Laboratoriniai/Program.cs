7c29b20dd0c28658340eb6b707180278024c920b
emirisu <emilis.mike@gmail.com>
2026-06-03 20:13:01 +0300

    api health check for Web API

---
+++ POA Laboratoriniai/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/health", () => "API is running and healthy" )
+    .WithName("APIHealth");
+
+app.MapGet("/api/hello", () => "Hello World!" )
+    .WithName("HelloWorld");
+
+app.Run();
